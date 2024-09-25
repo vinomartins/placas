1ca1fbdce0d74a0a1ddc4424007c506489ee752d
vinomartins <71186673+vinomartins@users.noreply.github.com>
2024-09-25 14:19:47 +0000

    Cria input de texto para adicionar a placa.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+ 
+st.write("""
+# My first app
+Hello *world!*
+""")
+ 
+def calculaPontuacao(placa):
+    return 'teste'
 
+st.text_input("Insira o número da placa", key="placa")
+if st.button("Calcular pontuação"):
+    calculaPontuacao(int(st.session.placa))
