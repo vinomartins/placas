86396cd829ae62b794af69c54830efb901ba72e7
vinomartins <71186673+vinomartins@users.noreply.github.com>
2024-09-25 17:16:03 +0000

    Cria funções para verificar se o número é um palindromo/quadrado perfeito.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,28 @@
 import streamlit as st
 import pandas as pd
- 
+import math 
+
 st.write("""
-# My first app
-Hello *world!*
+# Jogo das placas
 """)
- 
+
+def quadradoPerfeito(n):
+    return math.floor(math.sqrt(n))**2 == float(n)
+
+def palindromo(n):
+    return str(n) == str(n)[::-1]
+
 def calculaPontuacao(placa):
-    return 'teste'
+    points = placa
+    if quadradoPerfeito(placa):
+        st.info('Quadrado Perfeito! +5 pontos', icon=":material/thumb_up:")
+        points += 5
+    if palindromo(placa):
+        st.info('Palindromo! +3 pontos', icon="ℹ️")
+        points += 3
+    st.title(points)
+
 
 st.text_input("Insira o número da placa", key="placa")
 if st.button("Calcular pontuação"):
-    calculaPontuacao(int(st.session.placa))
+    calculaPontuacao(int(st.session_state.placa))
